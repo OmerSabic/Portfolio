f0279b20c65e1e66076e8fbdea8da35baabf821f
Omer Sabic <osabic2004@gmail.com>
2022-03-26 18:45:44 +0100

    fixed a lot of bugs

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const projects = [
         name: "Portfolio",
         description: "You are looking at it right now! A small website I made mainly to paractice css and learn more about databases. I use it to store projects I want to show off to others. ",
         icon: "https://i.imgur.com/d9b1CI9.png",
-        badges: ["html", "js", "css", "nodejs"],
+        badges: ["html", "javascript", "css", "nodejs"],
         links: ["https://github.com/OmerSabic/Portfolio"]
     },
     {
         name: "Pterodactyl Discord Bot",
         description: "Disocord bot used to manage minecraft servers that are using the Pterodactyl Panel. You can turn the server on or off, get RAM, CPU or Disk usage and get server IP and version. ",
         icon: "https://i.imgur.com/lrftl9O.png",
-        badges: "python",
+        badges: ["python"],
         links: ["https://github.com/OmerSabic/Pterodactyl-Discord-Bot"]
     }
 ]
@@ -31,20 +31,17 @@ app.get('/about', (req, res) => {
     res.render('about', { title: "About" })
 })
 
-app.get('/project/:projectName', async (req, res) => {
-    projects.forEach(project => {
-        if(project.name == req.params.projectName) {
-            return res.render('project', { data: JSON.stringify(det[0]) })
-        }
-    })
-
-    return res.send("404 not found")
+app.get('/project/:projectID', async (req, res) => {
+    if(projects[req.params.projectID]) {
+        res.render('project', { data: JSON.stringify(projects[req.params.projectID]) })
+    }
+    else {
+        res.send('404 not found')
+    }
 })
 
-app.get('/main', (req, res) => {
-    res.render('main')
-})
+const PORT = process.env.PORT || 3001;
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('server started');
 });
